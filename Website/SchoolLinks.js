7557f3b9afb47f5022a20edae4ce80fd30ce1764
Nerv3sine <44034888+Nerv3sine@users.noreply.github.com>
2021-05-18 23:00:45 -0700

    Update on school links redirecting mechanics

--- Website/SchoolLinks.js
+++ Website/SchoolLinks.js
@@ -2,16 +2,15 @@ const navigate = (page) => {
     switch(page){
         case 0:
             window.open('https://myexperience.sfu.ca/myAccount/dashboard.htm');
-            window.location.href = 'https://myexperience.sfu.ca/myAccount/dashboard.htm';
             break;
         case 1:
-            window.location.href = 'https://mail.sfu.ca';
+            window.open('https://mail.sfu.ca');
             break;
         case 2:
-            window.location.href = 'https://canvas.sfu.ca/';
+            window.open('https://canvas.sfu.ca/');
             break;
         case 3:
-            window.location.href = 'https://crowdmark.com/';
+            window.open('https://crowdmark.com/');
             break;
     }
 }
