91c4900c73c848a2013fe274632eca90045c252c
Nerv3sine <44034888+Nerv3sine@users.noreply.github.com>
2021-11-12 01:00:46 -0800

    banner implementation test

--- Website/MainPage.js
+++ Website/MainPage.js
@@ -1,4 +1,24 @@
+var bannerIndex = 0;
+showSlide(bannerIndex);
 
+function changeSlide(n){
+    showSlide(bannerIndex += n);
+}
+
+function showSlide(n){
+    var i;
+    var x = document.getElementsByClassName("banner-slides");
+    if(n > x.length){
+        bannerIndex = 0;
+    }
+    if(n < 0){
+        bannerIndex = x.length;
+    }
+    for(i = 0; i < x.length; i++){
+        x[i].style.display = "none";
+    }
+    x[bannerIndex].style.display = "block";
+}
 
 const navSFUCo_op = () => {
     window.location.href = 'https://myexperience.sfu.ca/myAccount/dashboard.htm';
