ce9151c0fdaf9b2c967370fa4cba9e431b30f28f
Nerv3sine <44034888+Nerv3sine@users.noreply.github.com>
2021-11-12 18:23:00 -0800

    html content adjustment mechanics implementation

--- Website/WIP.js
+++ Website/WIP.js
@@ -1,4 +1,25 @@
-let img1 = "https://i.pinimg.com/originals/8a/90/98/8a909882b3c6a2fcba6e1d4a42dabd42.jpg";
-let img2 = "https://wallpaperaccess.com/full/2029165.jpg";
-let img3 = "https://wallpaperaccess.com/full/2232415.jpg";
+let bannerPageIndex = 1;
 
+let bannerComponent = document.getElementsByClassName("img-content");
+
+let bannerContent = [
+    "https://i.pinimg.com/originals/8a/90/98/8a909882b3c6a2fcba6e1d4a42dabd42.jpg",
+    "https://wallpaperaccess.com/full/2029165.jpg",
+    "https://wallpaperaccess.com/full/2232415.jpg"
+]
+
+bannerUpdate();
+
+const bannerUpdate = () => {
+    bannerComponent.src = bannerContent[bannerPageIndex - 1];
+}
+
+const changePage = (n) => {
+    bannerPageIndex += n;
+    if(bannerPageIndex > bannerContent.length){
+        bannerPageIndex = 1;
+    }
+    if(bannerPageIndex < 1){
+        bannerPageIndex = bannerContent.length;
+    }
+}
