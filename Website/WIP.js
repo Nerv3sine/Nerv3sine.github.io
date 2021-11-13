f412c20c5e8d5adb990871267e61f99dcdc35296
jca465 <jason_cai_4@sfu.ca>
2021-11-12 18:58:25 -0800

    top banner mechanics fixed

--- Website/WIP.js
+++ Website/WIP.js
@@ -1,17 +1,13 @@
 let bannerPageIndex = 1;
 
-let bannerComponent = document.getElementsByClassName("img-content");
-
 let bannerContent = [
     "https://i.pinimg.com/originals/8a/90/98/8a909882b3c6a2fcba6e1d4a42dabd42.jpg",
     "https://wallpaperaccess.com/full/2029165.jpg",
     "https://wallpaperaccess.com/full/2232415.jpg"
 ]
 
-bannerUpdate();
-
 const bannerUpdate = () => {
-    bannerComponent.src = bannerContent[bannerPageIndex - 1];
+    document.getElementById("banner-display").src = bannerContent[bannerPageIndex - 1];
 }
 
 const changePage = (n) => {
@@ -22,4 +18,5 @@ const changePage = (n) => {
     if(bannerPageIndex < 1){
         bannerPageIndex = bannerContent.length;
     }
+    bannerUpdate();
 }
