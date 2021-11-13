4bd6e57c1acb81481d7a847fe9539e696b39e0d0
Nerv3sine <44034888+Nerv3sine@users.noreply.github.com>
2021-11-12 17:00:43 -0800

    adde more content and slight style adjustment

--- Website/WIP.js
+++ Website/WIP.js
@@ -1,3 +1,4 @@
 let img1 = "https://i.pinimg.com/originals/8a/90/98/8a909882b3c6a2fcba6e1d4a42dabd42.jpg";
 let img2 = "https://wallpaperaccess.com/full/2029165.jpg";
+let img3 = "https://wallpaperaccess.com/full/2232415.jpg";
 
