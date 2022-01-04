b644464476cdeca7a93a4f92bc164b1ec2bb2b54
Nerv3sine <jasoncai64@gmail.com>
2022-01-03 18:50:05 -0800

    renamed template file adjusted the json outputs for get more info

--- Website/experiments/expListing.py
+++ Website/experiments/expListing.py
@@ -5,6 +5,7 @@ mypath = "Website/experiments"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath,f))]
 
 outputList = []
+siteNames = []
 for file in onlyfiles:
     
     parts = file.split(".")
@@ -12,8 +13,14 @@ for file in onlyfiles:
     if(parts[1] == "html" and parts[0] != "expMain"):
         outputList.append(file)
 
+        with open(mypath + "/" + file) as f:
+            content = f.readlines()
+            target = content[0].split("@")
+            siteNames.append(target[1])
+
 outputDict = {
-    "sites" : outputList
+    "sites" : outputList,
+    "names" : siteNames
 }
 
 outputJSON = json.dumps(outputDict)
