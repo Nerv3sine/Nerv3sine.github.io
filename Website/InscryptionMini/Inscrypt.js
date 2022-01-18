8feb6ad0177aa9b53682fdea2aa89e01331c55b1
Nerv3sine <jasoncai64@gmail.com>
2022-01-18 15:57:01 -0800

    refactored, optimized and cleaned up

--- Website/InscryptionMini/Inscrypt.js
+++ Website/InscryptionMini/Inscrypt.js
@@ -1,51 +1,68 @@
+const fileSrc = "https://nerv3sine.github.io/Website/InscryptionMini/CardDatabase.json"
+
 let handComponents = [];
 let playerComponents = [];
 let opponentComponents = [];
 let revealComponents = [];
-let cardSelection = [0, 0];
-let currentHand = ["Wolf", "Bear", "Snake", "Mantis God"]
+
+let cardSelection = [-1, -1];
+
+let currentHand = []
+let playerCards = new Array(5)
+let opponentCards = new Array(5)
+let revealCards = new Array(5)
 
 //0: hand
 //1: player
 //2: opponent
 //3: reveal
 
+const request = async(filePath) => {
+    const response = await fetch(filePath);
+    const data = await response.json();
+    return data;
+}
+
+const cardLoad = async (filePath) => {
+    data = await request(filePath);
+    currentHand = data.Cards
+}
+
 window.onload=function(){
     setup()
 }
 
-const setup = () => {
+const setup = async () => {
+    await cardLoad(fileSrc)
     let x = document.getElementById("reveal");
     revealComponents = x.children
     x = document.getElementById("opponent")
     opponentComponents = x.children
     x = document.getElementById("player")
     playerComponents = x.children
-    handSetup();
+    currentHand && handSetup();
 }
 
 const handSetup = () => {
     let x = document.getElementById("hand");
 
     let i = 0;
-    currentHand.forEach(element => {
+    for(let item of currentHand){
         const card = document.createElement("div");
-        card.className = "slot"
+        card.classList.add("slot")
         let tempFunc = "slotInteract(0, " + i + ")"
         card.setAttribute("onclick", tempFunc);
-        card.style.borderStyle = "solid";
-        card.style.backgroundColor = "tan";
+        card.classList.add("card")
 
         const label = document.createElement("p");
-        label.innerHTML = element;
-        label.style.color = "black";
-        label.style.textAlign = "center";
+        label.innerHTML = item.Name;
+        label.classList.add("cardLabel")
         card.appendChild(label);
 
         x.appendChild(card);
         handComponents.push(card);
         i++;
-    });
+    }
 }
 
 const slotInteract = (group, index) => {
@@ -55,6 +72,7 @@ const slotInteract = (group, index) => {
     if(cardSelection[0] != -1 && cardSelection[1] != -1){
         slotStateChange(false, getComponent(cardSelection[0], cardSelection[1]))
     }
+
     cardSelection = [group, index];
     slotStateChange(true, getComponent(cardSelection[0], cardSelection[1]))
 }
@@ -74,14 +92,27 @@ const getComponent = (group, index) => {
 
 const slotStateChange = (mode, component) => {
     if(mode){
-        component.style.borderColor = "blue"
-        component.style.borderWidth = "4px";
+        component.classList.add("selected")
     }else{
-        component.style.borderColor = "white"
-        component.style.borderWidth = "2px";
+        component.classList.remove("selected")
+        //component.style.borderColor = "white"
+        //component.style.borderWidth = "2px";
+        
     }
 }
 
+const cancel = () => {
+    if(cardSelection[0] != -1 && cardSelection[1] != -1){
+        slotStateChange(false, getComponent(cardSelection[0], cardSelection[1]))
+    }
+    cardSelection = [-1, -1]
+}
+
+
+const test = () => {
+    let test = document.getElementById("test")
+
+}
 
 const p = (msg) => {
     console.log(msg)
