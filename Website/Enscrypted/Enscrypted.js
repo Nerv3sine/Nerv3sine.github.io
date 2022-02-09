d110dab66a226d3e725bd9bcb5c0df93de2cab55
Nerv3sine <jasoncai64@gmail.com>
2022-02-09 12:10:01 -0800

    added additional access points to the new page implemented a hand reset option

--- Website/Enscrypted/Enscrypted.js
+++ Website/Enscrypted/Enscrypted.js
@@ -1,3 +1,43 @@
+const fileSrc = "https://nerv3sine.github.io/Website/Enscrypted/CardDatabase.json";
+
+let starterSet = new Array();
+
+const requestInfo = async(filePath) => {
+    const response = await fetch(filePath);
+    const data = await response.json();
+    return data;
+}
+
+const getStarterSet = async(filePath) => {
+    data = await requestInfo(filePath);
+    for(let x = 0; x < 4; x++){
+        let card = Object.create(data.Cards[x]);
+        starterSet.push(card);
+    }
+    console.log("starter set retrieved!");
+}
+
 const enter = () => {
     window.location.href = 'EnscryptedBattle.html';
+    playerCardsCheck();
+}
+
+const scavenge = () => {
+    window.location.href = 'EnscryptPickup.html';
+    playerCardsCheck();
+}
+
+const playerCardsCheck = () => {
+    if(window.localStorage.length == 0){
+        resetCards();
+    }
+}
+
+const resetCards = () => {
+    window.localStorage.setItem("playerHand", starterSet);
+    console.log("hand reset");
+}
+
+window.onload=function(){
+    getStarterSet(fileSrc);
 }
