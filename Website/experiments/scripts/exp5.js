5024a86043695f5b940e51a01a6b37bfdcda11ac
Jason Cai <jasoncai64@gmail.com>
2023-05-09 02:05:44 -0700

    code rework and optimization
    optimized values for easier adjustability
    slightly optimized the animation code
    added in enum for different animation pattern cases
    other slight reworks for better readability

--- Website/experiments/scripts/exp5.js
+++ Website/experiments/scripts/exp5.js
@@ -1,5 +1,52 @@
-const colors = [[0, 118, 255], [81, 162, 255], [108, 255, 228], [45, 234, 91], [0, 221, 0]];
+const ShapeTypes = {
+    SINE: 1,
+    CIRCULAR: 2
+}
+
+const colors = [
+    [0, 118, 255], 
+    [81, 162, 255], 
+    [108, 255, 228], 
+    [45, 234, 91], 
+    [0, 221, 0]
+];
+
+const Params = {
+    "windowWidth": 100,
+    "refreshDelay": 20,
+    
+    "particleProperties": {
+        "progressLimit": 100,
+
+        "baseSize": 10,
+        "sizeRange": 10,
+
+        "baseSpeed": 0.0002,
+        "speedRange": 500,
+        "speedRangeNormalization": .000001,
+    },
+
+    "sinePattern": {
+        "resetOffset": 4,
+
+        "xOffsetRange": 30,
+
+        "xBaseStretch": 20,
+        "xStretchRange": 8,
+
+        "yBaseRestriction": 65,
+        "yRestrictionRange": 10
+    },
+
+    "circularPattern":{
+        "baseRadius": 1,
+        "radiusRange": 20,
+        "radiusRangeNormalization": 0.001
+    }
+}
+
 let circles = [];
+let currentShape = ShapeTypes.SINE;
 
 function getRandInt(max) {
     return Math.floor(Math.random() * max);
@@ -12,40 +59,65 @@ const sineAnimCalc = (x, horizontalOffset, horizontalStretch ,verticalSize) => {
 }
 
 window.onload=function(){
+    spawnParticles();
+    animateParticles();
+}
+
+const updateParticle = (particle) => {
+    
+    particle["progress"] += particle["speed"];
+    switch(currentShape){
+        case ShapeTypes.SINE:
+            if(particle["progress"] > Params["particleProperties"]["progressLimit"]){
+                particle["progress"] -= Params["particleProperties"]["progressLimit"] + Params["sinePattern"]["resetOffset"];
+            }
+            particle.style.left = particle["progress"] + "%";
+            particle.style.top = sineAnimCalc(particle["progress"], particle["offset"], particle["stretch"], particle["yRestrict"]) + "%";
+            break;
+        case ShapeTypes.CIRCULAR:
+            
+            break;
+        default:
+            console.log("currentShape has been set to an unknown value!");
+    }
+}
+
+const spawnParticles = () => {
     const canvas = document.getElementById("canvas");
 
     for(let i = 0; i < 100; i++){
+
         let circle = document.createElement("div");
         circle.className += "circle";
-        let size = getRandInt(10) + 10;
+        
+        let size = getRandInt(Params["particleProperties"]["sizeRange"]) + Params["particleProperties"]["baseSize"];
         circle.style.width = size + "px";
         circle.style.height = size + "px";
+
         let color = colors[getRandInt(colors.length)];
         circle.style.backgroundColor = "rgba(" + color[0] + "," + color[1] + "," + color[2] + "," + 0.3 + ")";
-        circle["x"] = getRandInt(100);
-        //circle["y"] = getRandInt(75);
-        circle["offset"] = getRandInt(30);
-        circle["stretch"] = 20 + getRandInt(8);
-        circle["yRestrict"] = 60 + getRandInt(15);
-        circle["speed"] = getRandInt(500);
-        circle.style.left = circle["x"] + "%";
-        circle.style.top = sineAnimCalc(circle["x"], circle["offset"], circle["stretch"], circle["yRestrict"]) + "%";
+        
+        //setting parameters
+        circle["progress"] = getRandInt(Params["particleProperties"]["progressLimit"]);
+        //circle["x"] = circle["progress"];
+        circle["offset"] = getRandInt(Params["sinePattern"]["xOffsetRange"]);
+        circle["stretch"] = Params["sinePattern"]["xBaseStretch"] + getRandInt(Params["sinePattern"]["xStretchRange"]);
+        circle["yRestrict"] = Params["sinePattern"]["yBaseRestriction"] + getRandInt(Params["sinePattern"]["yRestrictionRange"]);
+        
+        circle["speed"] = getRandInt(Params["particleProperties"]["speedRange"]) 
+        * Params["particleProperties"]["speedRangeNormalization"] 
+        + Params["particleProperties"]["baseSpeed"];
+
+        animateParticles(circle);
         circles.push(circle);
         canvas.appendChild(circle);
     }
-    
-    mainPageAnim();
-    
 }
 
-const mainPageAnim = () => {
-    for(let i = 0; i < circles.length; i++){
-        circles[i]["x"] += circles[i]["speed"]/100000 + 0.01;
-        if(circles[i]["x"] > 100){
-            circles[i]["x"] = -4;
-        }
-        circles[i].style.left = circles[i]["x"] + "%";
-        circles[i].style.top = sineAnimCalc(circles[i]["x"], circles[i]["offset"], circles[i]["stretch"], circles[i]["yRestrict"]) + "%";
-    }
-    setTimeout(mainPageAnim, 5);
+const animateParticles = () => {
+    
+    circles.forEach(particle => {
+        updateParticle(particle);
+    });
+    setTimeout(animateParticles, Params["refreshDelay"]);
 }
