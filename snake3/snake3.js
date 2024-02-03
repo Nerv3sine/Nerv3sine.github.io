d47495f8384f294a7208b3aca55f68d9934cc110
Jason Cai <jasoncai64@gmail.com>
2024-02-02 18:07:12 -0800

    added encapsulation changes and UI
    optimized and cleaned up the code with encapsulation for the snake
    added UI for pause and play

--- snake3/snake3.js
+++ snake3/snake3.js
@@ -1,150 +1,104 @@
 const UP = 0; const DOWN = 2;
 const LEFT = 1; const RIGHT = 3;
 
-var size = 25;
-var speed = -1;
-var spdVal = 1;
-var consumption = 25;
-var weight = 25;
-var x = 500;
-var y = 500;
-var direction = LEFT;
-var intention = LEFT;
+const PAUSE = 0; const PLAY = 1;
 
-var parts = [];
+var gameState = PAUSE;
+var cheats = true;
 
-var food = new v2(0, 0);
-var cheats = false;
-
+var snake;
+var interval = 10;
 var score = 0;
 
+var size = 25;
+var food = new v2(0, 0);
+
 function setup() {
-  createCanvas(windowWidth - 4, windowHeight - 4);
+  let canvas = createCanvas(windowWidth - 4, windowHeight - 4);
+  snake = new snek(interval);
   background(0, 100, 0);
-  parts.push(new section(new v2(x,y)));
+  
+  canvas.mouseClicked(function(){
+    if(gameState == PAUSE){
+      gameState = PLAY;
+    }
+  });
 }
 
 
 function draw() {
   background(0, 100, 0);
   
+  if(gameState == PAUSE){
+    paused();
+  }else{
+    game();
+  }
+}
+
+function paused(){
+  //UI
+  textAlign(CENTER);
+  fill(255);
+  stroke(255);
+  strokeWeight(5);
+  textSize(100);
+  text("Snake!", width/2, height/2 - 250);
+  strokeWeight(2);
+  textSize(75);
+  text("click the screen to start", width/2, height/2);
+}
+
+function game(){
+  //UI
+  textAlign(LEFT);
   fill(255);
   stroke(255);
   strokeWeight(1);
   textSize(50);
   text(score, width - 25 - 25 * ((score == 0) ? 1 : int(log(score)/log(10)) + 1), 50);
-  console.log()
-
+  
+  //food
   fill(200, 100, 100);
   noStroke();
   ellipse(food.x, food.y, size/2, size/2);
-  if(x == food.x && y == food.y){
-     eat();
+  if(snake.headCollision(food.x, food.y)){
+     snake.eat();
+     score++;
      food = genV2();
-   }
-   if(food.x == 0 && food.y == 0){
+  }
+  if(food.x == 0 && food.y == 0){
      food = genV2();
-   }
+  }
    
-   //snake movement
-   if(direction % 2 == 0)
-   {
-      y += speed;
-      //print(x, y, '\n');
-   }
-   else
-   {
-      x += speed; 
-      //print(x, y, '\n');
-   }
-   
-   //snake head update
-   parts[0].setDest(x,y);
-   //snake tail update
-   if(consumption == 0)
-   {
-     parts[parts.length - 1].move();
-   }
-   else
-   {
-     consumption--;
-   }
-   
-   //updates rendering
-   for(var i = 0; i < parts.length; i++){
-     parts[i].update();
-   }
-   
-   //updates everything after entering a "tile"
-   if(x % 25 == 0 && y % 25 == 0){
-     //adds segment on turns
-     if(direction != intention){
-       part = new section(new v2(x,y));
-       parts.splice(0, 0, part);
-       //prepares for new movement values
-       direction = intention;
-       speed = spdVal;
-       if(direction == UP || direction == LEFT){
-         speed *= -1;
-       }
-     }
-     
-     //removes end segment if no longer needed
-     if(parts[parts.length - 1].finished()){
-       parts.pop();
-     }
-     //print(coords.get(0).x, coords.get(0).y, '\n');
-   }
-}
-
-function eat(){
-  consumption += weight;
-  score++;
+  snake.update();
+  if(snake.dead()){
+    reset();
+  }
 }
 
 function genV2(){
-  pos = generate();
-  for(var i = 0; i < parts.length; i++){
-    if(parts[i].contains(pos.x, pos.y)){
-      pos = generate();
-      i = 0;
-    }
+  do
+  {
+    pos = generate();
   }
+  while(snake.contains(pos.x, pos.y));
   return pos;
 }
 
 function generate(){
-  return new v2(int(random(2, int(width / 25) - 2)) * 25, int(random(2, int(height / 25) - 2)) * 25);
+  return new v2(int(random(2, int(width / interval) - 2)) * interval, int(random(2, int(height / interval) - 2)) * interval);
 }
 
 function keyPressed(){
-  if(direction % 2 == 0)
-  {
-    if(keyCode == LEFT_ARROW)
-    {
-      intention = LEFT;
-    }
-    else if(keyCode == RIGHT_ARROW)
-    {
-      intention = RIGHT;
-    }
+  if(gameState == PLAY){
+    snake.keyCodeTrigger(keyCode, cheats);
   }
-  else
-  {
-    if(keyCode == UP_ARROW)
-    {
-      intention = UP;
-    }
-    else if(keyCode == DOWN_ARROW)
-    {
-      intention = DOWN;
-    }
-  }
-  if(cheats){
-    if(keyCode == 32)
-    {
-      eat();
-    }
-  }
-  //console.log(intention);
+}
+
+function reset(){
+  food = new v2(0, 0);
+  score = 0;
+  snake.reset();
+  gameState = PAUSE;
 }
---
+++ snake3/snakeManager.js
@@ -0,0 +1,140 @@
+class snek{
+  
+  spdVal = 1;
+  
+  setResetPos = function(){
+    this.x = int(width/2);
+    this.x = this.x - (this.x%this.interval);
+    this.y = int(height/2);
+    this.y = this.y - (this.y%this.interval);
+  }
+  
+  constructor(interval){
+    this.interval = interval;
+    
+    this.setResetPos();
+    
+    this.direction = LEFT;
+    this.intention = LEFT;
+    this.speed = -1;
+    
+    this.consumption = this.interval * 2;
+    
+    this.parts = [];
+    this.parts.push(new section(new v2(this.x,this.y)));
+  }
+  
+  update(){
+    //snake movement
+    if(this.direction % 2 == 0)
+    {
+       this.y += this.speed;
+       //print(x, y, '\n');
+    }
+    else
+    {
+       this.x += this.speed; 
+       //print(x, y, '\n');
+    }
+    
+    //snake head update
+    this.parts[0].setDest(this.x,this.y);
+    //snake tail update
+    if(this.consumption == 0)
+    {
+      this.parts[this.parts.length - 1].move();
+    }
+    else
+    {
+      this.consumption--;
+    }
+    
+    //updates rendering
+    for(var i = 0; i < this.parts.length; i++){
+      this.parts[i].update();
+    }
+    
+    //updates everything after entering a "tile"
+    if(this.x % this.interval == 0 && this.y % this.interval == 0){
+       
+       //adds segment on turns
+       if(this.direction != this.intention){
+         var part = new section(new v2(this.x,this.y));
+         this.parts.splice(0, 0, part);
+         
+         //prepares for new movement values
+         this.direction = this.intention;
+         this.speed = this.spdVal;
+         if(this.direction == UP || this.direction == LEFT){
+           this.speed *= -1;
+         }
+       }
+       
+       //removes end segment if no longer needed
+       if(this.parts[this.parts.length - 1].finished()){
+         this.parts.pop();
+       }
+    }
+  }
+  
+  headCollision(Nx, Ny){
+    return Nx == this.x && Ny == this.y;
+  }
+  
+  keyCodeTrigger(keyVal, cheats){
+    if(this.direction % 2 == 0)
+    {
+      if(keyVal == LEFT_ARROW)
+      {
+        this.intention = LEFT;
+      }
+      else if(keyVal == RIGHT_ARROW)
+      {
+        this.intention = RIGHT;
+      }
+    }
+    else
+    {
+      if(keyVal == UP_ARROW)
+      {
+        this.intention = UP;
+      }
+      else if(keyVal == DOWN_ARROW)
+      {
+        this.intention = DOWN;
+      }
+    }
+    if(cheats){
+      if(keyVal == 32)
+      {
+        this.eat();
+      }
+    }
+  }
+  
+  eat(){
+    this.consumption += this.interval;
+  }
+  
+  contains(Nx, Ny){
+    for(var i = 0; i < this.parts.length; i++){
+      if(this.parts[i].contains(Nx, Ny)){
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  reset(){
+    this.parts = [];
+    this.consumption = this.interval * 2;
+    this.setResetPos();
+    this.direction = LEFT;
+    this.intention = LEFT;
+    this.parts.push(new section(new v2(this.x,this.y)));
+  }
+  
+  dead(){
+    return this.x < 0 || this.x > width || this.y < 0 || this.y > height
+  }
+}
