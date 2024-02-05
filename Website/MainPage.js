9989192d630d9eaff87cc06ca1b455c7e26157a6
Nerv3sine <jasoncai64@gmail.com>
2024-02-05 13:14:50 -0800

    flexible web design changes
    stored all work info to be displayed in a json file
    changed the js file for the main page to load the information from the json file in a presentable format
    cleared out the original content in the html file for the new flexible web design
    Not completely finished, uploading for the sake of testing and polish

--- Website/MainPage.js
+++ Website/MainPage.js
@@ -1,21 +1,64 @@
-const navSFUCo_op = () => {
-    window.location.href = 'https://myexperience.sfu.ca/myAccount/dashboard.htm';
+const siteListFilePath = "https://nerv3sine.github.io/Website/ContentShowcase.json";
+// const siteListFilePath = "./ContentShowcase.json"
+
+const request = async (filePath) => 
+{
+    const response = await fetch(filePath);
+    const data = await response.json();
+    return data;
 }
 
-const navigate = (page) => {
-    switch(page){
-        case 0:
-            window.location.href = 'SchoolLinks.html';
-            break;
-        case 1:
-            window.location.href = 'WIP.html';
-            break;
-        case 2:
-            window.location.href = 'Enscrypted/Enscrypted.html';
-            break;
+const loadGallery = async () => 
+{
+    console.log("test");
+    data = await request(siteListFilePath);
+
+    let display = document.getElementById('gallery');
+
+    for(let content of data.content)
+    {
+        addToGallery(content, display);
     }
+   
 }
 
-const wOpen = (page) => {
+const addToGallery = (data, display) => 
+{
+    let work = document.createElement("div");
+    work.className = "item";
+    
+    let img = document.createElement("img");
+    img.src = data.image;
+    //CHANGE??
+    img.height = "225";
+    //
+    work.appendChild(img);
+
+    let caption = document.createElement("p");
+    caption.innerHTML = data.caption;
+    work.appendChild(caption);
+
+    let subCap = document.createElement("p");
+    subCap.className = "msg";
+    subCap.innerHTML = data.subCaption;
+    work.appendChild(subCap);
+
+    for(let b of data.buttons){
+        let btn = document.createElement("button");
+        btn.addEventListener("click", wOpen(b.link));
+        btn.className = "btn";
+        btn.innerHTML = b.label;
+        work.appendChild(btn);
+    }
+
+    display.appendChild(work);
+}
+
+const wOpen = (page) => 
+{
     window.open(page);
 }
+
+window.addEventListener("load", () => {
+    loadGallery();
+})
